39b379ec383b74aedd16ee810ce8eb08e89ac856
Richa-12y <kricha000@gmail.com>
2024-02-05 21:33:41 +0530

    add bookList and bookShow functionlity

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   const handleCreateBook = (title) => {
-    console.log(title, "Need to Add with");
+    // console.log(title, "Need to Add with");
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title: title },
+    ];
+    setBooks(updatedBooks);
   };
   return (
-    <div className="App">
+    <div className="app">
+      <BookList books={books} deleteBookById={deleteBookById} />
       <BookCreate handleCreateBook={handleCreateBook} />
     </div>
   );
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import BookEdit from "./BookEdit";
+const BookShow = ({ book, deleteBookById }) => {
+  const [showEdit, setShowEdit] = useState(false);
+  const handleDeleteClick = () => {
+    deleteBookById(book.id);
+  };
 
-const BookShow = () => {
-  return <div>BookShow</div>;
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit />;
+  }
+  return (
+    <div className="book-show">
+      {content}
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default BookShow;
